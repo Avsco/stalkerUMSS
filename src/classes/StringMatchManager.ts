b44c0857a38d227d980e8b65f5865a25fb8b2446
Avsco <benacho82@gmail.com>
2020-09-15 10:22:24 -0400

    fix modules of vuex and css

---
+++ src/classes/StringMatchManager.ts
@@ -0,0 +1,23 @@
+class StringMatchManager {
+    get = (searchedWord: string, wordsToCompare: string[]): string[] => {
+        const expresions = this.searchForWord(
+            searchedWord
+                .toLocaleUpperCase()
+                .split(' ')
+                .map((word) => RegExp(word))
+        )
+
+        const matchesOfWords = wordsToCompare.map((word) => expresions(word))
+        const maxCountOfMatches: number = Math.max(...matchesOfWords)
+
+        return wordsToCompare.filter((word, index) => matchesOfWords[index] == maxCountOfMatches)
+    }
+
+    searchForWord = (regseExp: RegExp[]) => (wordToCompare: string): number =>
+        regseExp.reduce((matches: number, regExp: RegExp) => {
+            if (regExp.test(wordToCompare.toLocaleUpperCase())) return matches + 1
+            return matches
+        }, 0)
+}
+
+export default StringMatchManager
