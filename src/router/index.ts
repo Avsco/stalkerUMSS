c44fbdc3132b5cb7a4aa1cfe902a927694153246
Avsco <benacho82@gmail.com>
2020-12-15 10:48:23 -0400

    delete: home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-// import Frontend from '@/Layouts/Frontend.vue'
-
-// import Stalker from '@/views/Stalker.vue'
-// import Cappuchino from '@/views/Cappuchino.vue'
-// import Menu from '@/views/deleteMe.vue'
-
 Vue.use(VueRouter)
 const router = new VueRouter({
     mode: 'history',
@@ -20,11 +14,6 @@ const router = new VueRouter({
             children: [
                 {
                     path: '',
-                    name: 'menu',
-                    component: () => import('@/views/deleteMe.vue')
-                },
-                {
-                    path: 'stalker',
                     name: 'Docentes',
                     component: () => import('@/views/Stalker.vue')
                 },
